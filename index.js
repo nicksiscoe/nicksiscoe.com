b9a30117e7e83fee77ae37d15cbc5677a3bda41f
Nick Siscoe <nick.siscoe@gmail.com>
2019-04-23 00:10:43 -0500

    its done

--- index.js
+++ index.js
@@ -1,4 +1,29 @@
-window.onload = function loadDate() {
-  var options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
-  document.getElementById("date").innerHTML = 'Lincoln, NE - ' + new Date().toLocaleDateString('en-US', options);
+function setMeImg() {
+  var parallax = document.querySelectorAll("body"), speed = 0.9;
+
+   [].slice.call(parallax).forEach(function(el,i){
+
+     windowYOffset = window.pageYOffset,
+         elBackgrounPos = "50% " + (windowYOffset * speed) + "px";
+
+     el.style.backgroundPosition = elBackgrounPos;
+
+   });
+
+   window.onscroll = function(){
+     [].slice.call(parallax).forEach(function(el,i){
+
+       windowYOffset = window.pageYOffset,
+           elBackgrounPos = "50% " + (windowYOffset * speed) + "px";
+
+       el.style.backgroundPosition = elBackgrounPos;
+
+     });
+   };
+
+
+  let rand = Math.floor(Math.random() * 14) + 1;
+  document.getElementById("me").src = `./img/me/me${rand}.png`
 }
+
+window.onload = setMeImg;
