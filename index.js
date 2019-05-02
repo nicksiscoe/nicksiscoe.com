8bdd5896e41ab241a48f05f7f08e10a3c46d494d
Nick Siscoe <nick.siscoe@gmail.com>
2019-05-02 16:27:31 -0500

    better site (no more weird scrolling) + updates

--- index.js
+++ index.js
@@ -1,27 +1,4 @@
 function setMeImg() {
-  var parallax = document.querySelectorAll("body"), speed = 0.85;
-
-   [].slice.call(parallax).forEach(function(el,i){
-
-     windowYOffset = window.pageYOffset,
-         elBackgrounPos = "50% " + (windowYOffset * speed) + "px";
-
-     el.style.backgroundPosition = elBackgrounPos;
-
-   });
-
-   window.onscroll = function(){
-     [].slice.call(parallax).forEach(function(el,i){
-
-       windowYOffset = window.pageYOffset,
-           elBackgrounPos = "50% " + (windowYOffset * speed) + "px";
-
-       el.style.backgroundPosition = elBackgrounPos;
-
-     });
-   };
-
-
   let rand = Math.floor(Math.random() * 20) + 1;
   document.getElementById("me").src = `./img/me/me${rand}.png`
 }
