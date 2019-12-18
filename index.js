9d9cede3cb146f85388f84a3a81a2e037bf1449b
Nick Siscoe <nick.siscoe@gmail.com>
2019-12-18 12:41:13 -0600

    new layout

--- index.js
+++ index.js
@@ -3,4 +3,26 @@ function setMeImg() {
   document.getElementById("me").src = `./img/me/me${rand}.png`
 }
 
-window.onload = setMeImg;
+function adjustWidth() {
+  var parentwidth = $(".parent").width();
+  var windowwidth = $(window).width();
+  if (windowwidth >= 992) {
+    $("header").width(parentwidth);
+  } else {
+    $("header").css('width', '100%');
+  }
+}
+
+$(function() {
+
+$(window).resize(
+  function() {
+    adjustWidth();
+  })
+
+})
+
+window.onload = function(e){
+  setMeImg();
+  adjustWidth();
+}
